73f43dae241198ad092dd006c08014bb63df355d
Steven <stevensameh2010@hotmail.com>
2022-12-21 12:07:58 +0200

    Detect Enter Pressed in Search Fragment, extract its value

--- app/src/main/java/com/stivoo/wagba/ui/home/search/SearchFragment.java
+++ app/src/main/java/com/stivoo/wagba/ui/home/search/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.stivoo.wagba.ui.home.search;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,9 +14,13 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.stivoo.wagba.R;
@@ -32,6 +37,8 @@ public class SearchFragment extends Fragment implements FeaturedRestaurantsRecyc
 
     ArrayList<RestaurantModel> restaurants;
     SearchSimulationAdapter adapter = new SearchSimulationAdapter(this);
+
+    EditText search_val;
 
 
     public SearchFragment() {
@@ -80,6 +87,21 @@ public class SearchFragment extends Fragment implements FeaturedRestaurantsRecyc
         recycler.setNestedScrollingEnabled(false);
         recycler.setAdapter(adapter);
         recycler.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        search_val = view.findViewById(R.id.sfsearch_txt);
+        search_val.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    Toast.makeText(getContext(), search_val.getText().toString(), Toast.LENGTH_SHORT).show();
+                    InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(search_val.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 
     @Override
