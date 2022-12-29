fbb4f8d1e41b9feeaa7037ee6ba2bf5ae7559dc6
Steven <stevensameh2010@hotmail.com>
2022-12-29 13:37:27 +0200

    Connected increment and decrementing quantity in cart with firebase

--- app/src/main/java/com/stivoo/wagba/ui/home/cart/CartAdapter.java
+++ app/src/main/java/com/stivoo/wagba/ui/home/cart/CartAdapter.java
@@ -3,13 +3,21 @@ package com.stivoo.wagba.ui.home.cart;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.stivoo.wagba.R;
 import com.stivoo.wagba.pojo.CartItem;
 import com.stivoo.wagba.pojo.MealModel;
@@ -19,7 +27,7 @@ import java.util.ArrayList;
 
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
 
-    private ArrayList<CartItem> cartMealsList = new ArrayList<>();
+    private static ArrayList<CartItem> cartMealsList = new ArrayList<>();
 
     @NonNull
     @Override
@@ -47,12 +55,14 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
         notifyDataSetChanged();
     }
 
-    public static class CartViewHolder extends RecyclerView.ViewHolder {
+    public class CartViewHolder extends RecyclerView.ViewHolder {
 
         ImageView img;
         TextView price;
         TextView name;
         TextView qty;
+        Button inc;
+        Button dec;
 
         public ImageView getImg() {
             return img;
@@ -65,7 +75,85 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
             price = itemView.findViewById(R.id.ctv_meal_price_val);
             name = itemView.findViewById(R.id.ctv_meal_name);
             qty = itemView.findViewById(R.id.ctv_quantity);
+            inc = itemView.findViewById(R.id.cbtn_inc);
+            dec = itemView.findViewById(R.id.cbtn_dec);
+
+
+            inc.setOnClickListener(v -> {
+                    qty.setText(String.valueOf(Integer.parseInt(qty.getText().toString()) + 1));
+                    DecimalFormat df = new DecimalFormat("#.00");
+                    Float price_val = (Float.parseFloat(price.getText().toString())/(Integer.parseInt(qty.getText().toString())-1)) * Integer.parseInt(qty.getText().toString());
+                    price.setText(df.format(price_val));
+
+                final DatabaseReference CART_REF = FirebaseDatabase.getInstance().getReference("/CurrentCart");
+                final DatabaseReference CHECK_REF =
+                        FirebaseDatabase.getInstance().getReference("/CurrentCart"+"/"+FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                ValueEventListener eventListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()) {
+                            CART_REF.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(name.getText().toString()).child("qty").setValue(Integer.parseInt(qty.getText().toString()));
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) { }
+                };
+                CHECK_REF.child(name.getText().toString()).addListenerForSingleValueEvent(eventListener);
+
+            });
+
+            dec.setOnClickListener(v -> {
+                qty.setText(String.valueOf(Integer.parseInt(qty.getText().toString()) - 1));
+                DecimalFormat df = new DecimalFormat("#.00");
+                Float price_val = (Float.parseFloat(price.getText().toString())/(Integer.parseInt(qty.getText().toString())+1)) * Integer.parseInt(qty.getText().toString());
+                price.setText(df.format(price_val));
+
+                final DatabaseReference CART_REF = FirebaseDatabase.getInstance().getReference("/CurrentCart");
+                final DatabaseReference CHECK_REF =
+                        FirebaseDatabase.getInstance().getReference("/CurrentCart"+"/"+FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                ValueEventListener eventListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()) {
+                            CART_REF.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(name.getText().toString()).child("qty").setValue(Integer.parseInt(qty.getText().toString()));
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) { }
+                };
+                CHECK_REF.child(name.getText().toString()).addListenerForSingleValueEvent(eventListener);
+
+                if(Integer.parseInt(qty.getText().toString()) == 0) {
+                    int pos = getAdapterPosition();
+                    removeAt(pos);
+
+                    ValueEventListener eventListener2 = new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.exists()) {
+                                CART_REF.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(name.getText().toString()).removeValue();
+                            }
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) { }
+                    };
+                    CHECK_REF.child(name.getText().toString()).addListenerForSingleValueEvent(eventListener2);
+
+
+                }
+            });
+
 
         }
+
+        private void removeAt(int position) {
+            cartMealsList.remove(position);
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position, cartMealsList.size());
+        }
+
+
     }
 }
